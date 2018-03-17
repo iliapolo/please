ade103b69fb8930aa5a74e4ee1333e90d4e3de41
iliapolo <eli.polonsky@gmail.com>
2018-03-17 12:44:49 +0200

    added setup-directories script

---
+++ macOS/setup-directories.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This script will create the dev directories
+
+whoami=$(whoami)
+
+mkdir -p /Users/${whoami}/dev/src/iliapolo
+mkdir -p /Users/${whoami}/dev/tools
+mkdir -p /Users/${whoami}/dev/runtime
