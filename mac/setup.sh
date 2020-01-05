b6c8c7797ba989919d1cf4bc4eca29b558008845
iliapolo <eli.polonsky@gmail.com>
2020-01-05 14:05:12 +0200

    mid work

--- mac/setup.sh
+++ mac/setup.sh
@@ -20,3 +20,26 @@ ${script_dir}/setup-apps.sh
 
 
 brew cask install visual-studio-code
+
+brew install task
+
+brew install gnupg
+
+brew install wget
+
+brew install rbenv
+
+brew tap AdoptOpenJDK/openjdk
+
+brew cask install adoptopenjdk8
+
+java installed under /Library/Java/JavaVirtualMachines/
+
+enable inifnite scroll in iterm
+
+vscode extenstions:
+
+- Docker
+- TSlint
+- Jest runner
+- Python
